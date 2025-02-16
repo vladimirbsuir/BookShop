e68227fb51e7eb6661685423b1200d05ba5d9601
vladimirbsuir <vladimirbsuir@gmail.com>
2025-02-16 21:17:07 +0300

    Initial commit

---
+++ src/main/java/com/example/bookshop/BookShopApplication.java
@@ -0,0 +1,15 @@
+package com.example.bookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** Main class of the app. */
+@SpringBootApplication
+public class BookShopApplication {
+
+    /** Main function of the app. */
+    public static void main(String[] args) {
+        SpringApplication.run(BookShopApplication.class, args);
+    }
+
+}
